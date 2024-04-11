45ae460fb3db86f157dda06144764c29a03fb26c
Danilosrr <danilo_srr@hotmail.com>
2024-04-11 03:21:17 -0300

    custom title bar

---
+++ src/components/titleBar/TitleBar.tsx
@@ -0,0 +1,40 @@
+import { appWindow } from "@tauri-apps/api/window";
+import "./TitleBar.css";
+
+interface TitleBarButtonProps {
+  onClick: () => void;
+  altText: string;
+  iconUrl: string;
+}
+
+function TitleBarButton({onClick,altText,iconUrl}: Readonly<TitleBarButtonProps>) {
+  return (
+    <button className="titlebar-button" onClick={onClick}>
+      <img src={iconUrl} alt={altText} />
+    </button>
+  );
+}
+
+function TitleBar() {
+  return (
+    <div data-tauri-drag-region className="titlebar">
+      <TitleBarButton
+        onClick={() => appWindow.minimize()}
+        altText="minimize"
+        iconUrl="https://api.iconify.design/mdi:window-minimize.svg"
+      />
+      <TitleBarButton
+        onClick={() => appWindow.maximize()}
+        altText="maximize"
+        iconUrl="https://api.iconify.design/mdi:window-maximize.svg"
+      />
+      <TitleBarButton
+        onClick={() => appWindow.close()}
+        altText="close"
+        iconUrl="https://api.iconify.design/mdi:close.svg"
+      />
+    </div>
+  );
+}
+
+export default TitleBar;
